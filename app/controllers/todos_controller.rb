f812cc07dce87864f19edd3c07cbefd7b3bc6d9d
IlyaSedov <i.sedov1996@gmail.com>
2019-12-01 22:29:01 +0300

    fix bugs form_for

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,14 @@
 class TodosController < ApplicationController
+  
+def new
+  @todo = Todos.new
+end
   def index
-    
+    @todos= Todo.all
   end
 
   def create
+    @todo = Todos.new
   end
 
   def delete
