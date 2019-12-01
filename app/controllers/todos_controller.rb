b02fd897e2d052ea40bdbf69ba4ffbcf90a0d873
IlyaSedov <i.sedov1996@gmail.com>
2019-12-01 16:33:09 +0300

    first commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,17 @@
+class TodosController < ApplicationController
+  def index
+    
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'todos#index'
+  
+  get 'todos/create'
+  get 'todos/delete'
+  get 'todos/edit'
+  get 'todos/update'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
