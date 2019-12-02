8451b7065d7724b674468c317514b82d7409b203
IlyaSedov <i.sedov1996@gmail.com>
2019-12-03 00:59:28 +0300

    changed the todos_controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,34 @@
 class TodosController < ApplicationController
   
-def new
-  @todo = Todos.new
-end
+# def new
+#   @todo = Todo.new(todo_params)
+# end
   def index
     @todos= Todo.all
   end
 
   def create
-    @todo = Todos.new
+     @todo = Todo.new(todo_params)#Todos.new?
+     if @todo.save
+        redirect_to root_path
+     else
+        redirect_to root_path
+     end
   end
 
   def delete
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to root_path
   end
 
   def edit
   end
 
   def update
+  end
+  private
+  def todo_params
+    params.require(:todo).permit(:title,:description,:completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'todos#index'
+  resources :todos
   
-  get 'todos/create'
-  get 'todos/delete'
-  get 'todos/edit'
-  get 'todos/update'
+  
+  # get 'todos/create'
+  # get 'todos/delete'
+  # get 'todos/edit'
+  # get 'todos/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
